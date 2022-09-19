2dd13b8240ff381cefd6343423c919d3d4b8f404
Christopher Wells <113203157+Chrisp1776@users.noreply.github.com>
2022-09-19 13:23:31 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Triangle_Farm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("What is the base of the triangle?");
+            Float Base = Console.ReadLine() ;
+            Convert.ToDouble(Base);
+            
+
+            Console.WriteLine("Now what is the height?");
+            Float Height = Console.ReadLine();
+            Convert.ToDouble(Height);
+
+            Base* Height;
+
+
+
+
+        }
+    }
+}
